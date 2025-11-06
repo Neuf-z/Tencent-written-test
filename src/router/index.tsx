449e87335fb224073470f8a8228f7cf1db21d83b
Neuf-z <zxf_0924@163.com>
2025-11-06 10:15:04 +0800

    删除多余代码

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,7 +4,6 @@ import { lazy } from "react";
 
 const Home = lazy(() => import("../pages/Home"));
 const NotFound = lazy(() => import("../pages/NotFound"));
-const Symposium = lazy(() => import("../pages/Symposium"));
 
 const routes: RouteObject[] = [
   {
